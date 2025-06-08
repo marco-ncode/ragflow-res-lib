76edee619361e3345c50328d24a9a2a12410eb39
Marco Bellante <103267756+marco-ncode@users.noreply.github.com>
2025-06-08 17:58:24 +0200

    Update index.tsx

--- web/src/layouts/components/right-toolbar/index.tsx
+++ web/src/layouts/components/right-toolbar/index.tsx
@@ -71,17 +71,8 @@ const RightToolBar = () => {
   return (
     <div className={styled.toolbarWrapper}>
       <Space wrap size={16}>
-        <Dropdown menu={{ items, onClick: handleItemClick }} placement="bottom">
-          <Space className={styled.language}>
-            <b>{t(camelCase(language))}</b>
-            <DownOutlined />
-          </Space>
-        </Dropdown>
         <Circle>
           <GithubOutlined onClick={handleGithubCLick} />
-        </Circle>
-        <Circle>
-          <CircleHelp className="size-4" onClick={handleDocHelpCLick} />
         </Circle>
         <Circle>
           {theme === 'dark' ? (
